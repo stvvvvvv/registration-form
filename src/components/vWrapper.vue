dbbdfbc8d1a16319dd8c5549775b611a7005a794
stvvvvvv <stv5458@gmail.com>
2021-07-27 19:08:50 +0300

    New
    1. Сreated app structure

---
+++ src/components/vWrapper.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="wrapper">
+    <h1>Wrapper</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'vWrapper'
+}
+</script>
+
+<style scoped lang="scss">
+</style>
--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import '@/assets/styles/main.scss'
 import { createApp } from 'vue'
 import App from './App.vue'
 
