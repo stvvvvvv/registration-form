f177de4ff0e4f764d483aa580d1922e67690cc2c
stvvvvvv <stv5458@gmail.com>
2021-08-06 14:18:26 +0300

    New
    1. Added input mask for Number field
    2. Added last step with refresh button

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
+import 'primevue/resources/primevue.min.css'
 import '@/assets/styles/main.scss'
+
 import { createApp } from 'vue'
 import App from './App.vue'
+import PrimeVue from 'primevue/config'
 
-createApp(App).mount('#app')
+createApp(App).use(PrimeVue).mount('#app')
